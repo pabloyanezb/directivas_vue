05ec33df168f73c35b3f04967a2feaed9a662bec
pabloyanezb <pabloyanezbarriga@gmail.com>
2020-07-09 13:14:13 -0400

    ADD: Reestructuración y limpieza de código

---
+++ script.js
@@ -0,0 +1,9 @@
+var app = new Vue({     
+    el: ".app",
+    data: {
+        name: 'Pablo',
+        encabezado: true,
+        Aprendidos: ['HTML', 'CSS', 'Javascript', 'JQuery', 'ES6'],
+        Estudiar: ['Vue', 'Python', 'React', 'Angular' ]
+    }
+})
